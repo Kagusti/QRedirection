4e1cc9016d238907edfd1cdb7612301f0314d19a
agustitru <atrullen1998@gmail.com>
2023-11-09 10:20:38 +0100

    V2.0 One Link for eHealth and SM

--- Controllers/QRedirectionController.cs
+++ Controllers/QRedirectionController.cs
@@ -7,20 +7,27 @@ namespace QRedirection.Controllers
     public class RedirectController : Controller
     {
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string? target = null) // Permitir que 'target' sea nulo
         {
             string userAgent = Request.Headers["User-Agent"].ToString();
+            string keitoeHealthAppleUrl = "https://apps.apple.com/es/app/keito-ehealth/id1546250400";
+            string keitoeHealthGooglePlayUrl = "https://play.google.com/store/apps/details?id=com.keito.ehealth.app";
 
-            if (userAgent.Contains("Macintosh") || userAgent.Contains("iPhone") || userAgent.Contains("iPad") || userAgent.Contains("iPod"))
+            // Verificar si el User-Agent corresponde a un dispositivo Apple
+            bool isAppleDevice = userAgent.Contains("Macintosh") || userAgent.Contains("iPhone") || userAgent.Contains("iPad") || userAgent.Contains("iPod");
+
+            // Redirigir a las URLs de keitoeHealth si 'target' es nulo o vacío, o si no es 'sm'
+            if (string.IsNullOrEmpty(target) || !target.Equals("sm", StringComparison.OrdinalIgnoreCase))
             {
-                // Redireccionar a la App Store
-                return Redirect("https://apps.apple.com/es/app/keito-ehealth/id1546250400");
+                return isAppleDevice ? Redirect(keitoeHealthAppleUrl) : Redirect(keitoeHealthGooglePlayUrl);
             }
-            else
-            {
-                // Redireccionar a Google Play (para dispositivos Android y Windows)
-                return Redirect("https://play.google.com/store/apps/details?id=com.keito.ehealth.app");
-            }
+
+            // Si 'target' es 'sm', redirigir a las URLs específicas de SM
+            string SMAppleUrl = "https://apps.apple.com/us/app/sm-demo-by-keito/id6470201705";
+            string SMGooglePlayUrl = "https://play.google.com/store/apps/details?id=com.keito.smdemo.app";
+            return isAppleDevice ? Redirect(SMAppleUrl) : Redirect(SMGooglePlayUrl);
         }
     }
 }
+
+
